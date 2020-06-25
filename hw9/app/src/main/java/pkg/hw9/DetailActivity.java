75b2697b7796e6c0b63218ed24d8529e02dbf644
Mingyu Cui <mingyucu@usc.edu>
2020-06-25 00:50:29 -0700

    before branching to write fragments from scratch

--- hw9/app/src/main/java/pkg/hw9/DetailActivity.java
+++ hw9/app/src/main/java/pkg/hw9/DetailActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -35,5 +36,10 @@ public class DetailActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
