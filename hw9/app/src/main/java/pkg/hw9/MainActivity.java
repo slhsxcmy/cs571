176082365dc3899ae669fa6e425158d046db71f2
Mingyu Cui <mingyucu@usc.edu>
2020-06-24 22:27:07 -0700

    clear button OK

--- hw9/app/src/main/java/pkg/hw9/MainActivity.java
+++ hw9/app/src/main/java/pkg/hw9/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText keywordField;
@@ -15,7 +19,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText maxField;
     private TextView keywordError;
     private TextView rangeError;
+    private CheckBox checkboxNew;
+    private CheckBox checkboxUsed;
+    private CheckBox checkboxUnspecified;
     private Spinner sortSpinner;
+    private Button searchButton;
+    private Button clearButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +44,58 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sort_options, R.layout.support_simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         sortSpinner.setAdapter(adapter);
-//        sortSpinner.setOnItemClickListener(this);
 
+        checkboxNew = findViewById(R.id.checkboxNew);
+        checkboxUsed = findViewById(R.id.checkboxUsed);
+        checkboxUnspecified = findViewById(R.id.checkboxUnspecified);
+
+        searchButton = findViewById(R.id.searchButton);
+        clearButton = findViewById(R.id.clearButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (validateKeyword() && validateRange()) {
+                    String url = genURL();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please fix all fields with errors", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                keywordField.setText("");
+                minField.setText("");
+                maxField.setText("");
+                checkboxNew.setChecked(false);
+                checkboxUsed.setChecked(false);
+                checkboxUnspecified.setChecked(false);
+                sortSpinner.setSelection(0);
+
+            }
+        });
+
+    }
+
+    private String genURL() {
+
+        String baseurl = "https://hw8-server-cs571su2020.wl.r.appspot.com/query?";
+
+        int filter_index = 0;
+        String query_string = "";
+
+        String kw = keywordField.getText().toString();
+        query_string += "keywords=" + keywordField.getText().toString();
+
+        return baseurl + query_string;
     }
 
     private boolean validateKeyword() {
         String keyword = keywordField.getText().toString().trim();
         if (keyword.isEmpty()) {
-            keywordField.setError("Please enter mandatory field");
+            keywordError.setVisibility(View.VISIBLE);
             return false;
         } else {
-            keywordField.setError("null");
+            keywordError.setVisibility(View.GONE);
             return true;
         }
     }
@@ -54,15 +104,32 @@ public class MainActivity extends AppCompatActivity {
         String fromS = minField.getText().toString().trim();
         String toS = maxField.getText().toString().trim();
         double from = 0, to = Double.MAX_VALUE;
-        try {
-            from = Double.parseDouble(fromS);
-            if (from < 0) throw new NumberFormatException();
-
-        } catch (NumberFormatException e) {
-            System.out.println("String " + from + "is not a number");
+        if (!fromS.isEmpty()) {
+            try {
+                from = Double.parseDouble(fromS);
+                if (from < 0) throw new NumberFormatException();
+            } catch (NumberFormatException e) {
+                rangeError.setVisibility(View.VISIBLE);
+                return false;
+            }
+        }
+        if (!toS.isEmpty()) {
+            try {
+                to = Double.parseDouble(toS);
+                if (to < 0) throw new NumberFormatException();
+            } catch (NumberFormatException e) {
+                rangeError.setVisibility(View.VISIBLE);
+                return false;
+            }
         }
 
-        return true;
+        if (!fromS.isEmpty() && !toS.isEmpty() && from > to) {
+            rangeError.setVisibility(View.VISIBLE);
+            return false;
+        } else {
+            rangeError.setVisibility(View.GONE);
+            return true;
+        }
 
     }
 
