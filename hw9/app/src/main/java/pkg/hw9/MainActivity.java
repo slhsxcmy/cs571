a01c5d75e80aef979906173060ddd3c19e0f816b
Mingyu Cui <mingyucu@usc.edu>
2020-06-24 19:13:14 -0700

    icon, splash, main toolbar OK

--- hw9/app/src/main/java/pkg/hw9/MainActivity.java
+++ hw9/app/src/main/java/pkg/hw9/MainActivity.java
@@ -1,6 +1,7 @@
 package pkg.hw9;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle("eBay Catalog"); //string is custom name you want
     }
 }
---
+++ hw9/app/src/main/java/pkg/hw9/SplashScreen.java
@@ -0,0 +1,20 @@
+// https://www.youtube.com/watch?v=o6_IEF7Q19o
+//
+package pkg.hw9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
